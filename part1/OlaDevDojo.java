9d22fb60630b5c302828109468722fc8f72e7c33
Sandro Augusto <sandroaugusto2895@gmail.com>
2024-05-14 01:54:58 -0300

    att

---
+++ part1/OlaDevDojo.java
@@ -0,0 +1,9 @@
+
+public class OlaDevDojo {
+	public static void main (String[] args){  
+		System.out.println("Ola Mundo!"); <--Assim é o print,  O ponto e virgula indica que a linha acabou.
+	}
+}
+
+
+
