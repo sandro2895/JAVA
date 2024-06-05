b994463e8b85e99aa9b5a4a68eebe8842cc4308d
Sandro Augusto <sandroaugusto2895@gmail.com>
2024-06-05 04:23:05 -0300

    att

--- part1/Pasta IDE/Projeto/src/academia/pacote/Aula2TiposPrimitivos.java
+++ part1/Pasta IDE/Projeto/src/academia/pacote/Aula2TiposPrimitivos.java
@@ -3,20 +3,24 @@ package academia.pacote;
 public class Aula2TiposPrimitivos {
     public static void main(String[] args) {
 // int, double, float, char, byte, short, long, boolean. 8 no total.
-        int myAge = 28;
-        long numeroGrande = 10000L;
+        int myAge = (int) 28888888888L;
+        long numeroGrande = (long) 155.23;
         double salarioDouble = 2000.50D;
-        float salarioFloat = 2500.0f;
+        float salarioFloat = (float) 2500.0d;
         byte idadeByte = 127;
         short idadeShort = 28;
         boolean verdadeiro = true;
         boolean falso = false;
         char caractere = 37;
+        var num = 30;
         System.out.println("Minha idade é " + myAge);
-        String myName = "doca";
+        String myName = "Doca Augusto";
         System.out.println("Meu nome é "+ myName);
         System.out.println(verdadeiro);
         System.out.println(caractere);
+        System.out.println(salarioFloat);
+        System.out.println(numeroGrande);
+        System.out.println(num);
 
     }
 }
